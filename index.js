626ed31776c3148c95c745be8e6b1b2e485f7449
miqueiasbelfort <kawasousa1554@gmail.com>
2022-07-16 17:15:59 -0300

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 //solve CORS
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+app.use(cors({credentials: true, origin: 'http://127.0.0.1:5173'}))
 
 //upload directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
