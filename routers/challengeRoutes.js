93d885b1caa2c449c2cd50abbae388ffabc3ad13
miqueiasbelfort <kawasousa1554@gmail.com>
2022-07-31 13:36:08 -0300

    delete edit & get

--- routers/challengeRoutes.js
+++ routers/challengeRoutes.js
@@ -63,6 +63,85 @@ router.post("/create/:guildId", verifyToken, async(req, res) => { //CREATEA A CH
 
 })
 
+router.patch("/edit/:id", verifyToken, async(req, res) => { //EDIT A CHALLENGE
 
+    const {id} = req.params
+
+    const {title, desc, score} = req.body
+
+    //get user by token
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    // get guild by user
+    const guild = await Guild.findOne({userName: user.username})
+
+    // get challenge by id
+    const challenge = await Challenges.findById(mongoose.Types.ObjectId(id))
+
+    if(!title){
+        return res.status(422).json({error: "O Titulo é obrigatorio!"})
+    } else if (!desc){
+        return res.status(422).json({error: "Por favor descreva o seu desafio!"})
+    } else if (!score){
+        return res.status(422).json({error: "De um valor para esse desafio!"})
+    }
+
+    if(challenge.guildName !== guild.guildname){
+        return res.status(422).json({error: "Algo está errado por favor tente novamente mais tarde!"})
+    } else if(!challenge){
+        return res.status(404).json({error: "Desafio não encontrado."})
+    }   
+
+    challenge.title = title
+    challenge.desc = desc
+    challenge.score = score
+
+    try {
+
+        challenge.save()
+        res.status(200).json({message: "Desafio atualizado com sucesso!"})
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
+router.delete("/:id", verifyToken, async(req, res) => { //DELETE A CHALLENGE
+
+    const {id} = req.params
+
+    //get user by token
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    // get guild by user
+    const guild = await Guild.findOne({userName: user.username})
+
+    // get challenge by id
+    const challenge = await Challenges.findById(mongoose.Types.ObjectId(id))
+    
+    if(!challenge){
+        return res.status(404).json({error: "Desafio não encontrado."})
+    }
+
+    try {
+
+        await Challenges.findByIdAndDelete(id)
+        res.status(200).json({message: "Desafio excluido com sucesso."})
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
+router.get("/", verifyToken, async(req, res) => { //GET ALL CHALLENGES
+
+    const challenges = await Challenges.find({})
+    res.status(200).json(challenges)
+
+})
 
 module.exports = router
